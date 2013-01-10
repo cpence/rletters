a02da4e8a8d1e021faaaaf9566c4c97cb85a240b
Charles Pence <charles@charlespence.net>
2013-01-10 16:28:56 -0500

    Add full support for editing app-wide settings in the admin panel.

--- app/admin/settings.rb
+++ app/admin/settings.rb
@@ -1,12 +1,5 @@
 # -*- encoding : utf-8 -*-
 
-class SettingValue < Struct.new(:key, :val)
-  include ActiveModel::Conversion  
-  extend ActiveModel::Naming
-    
-  def persisted?; false; end
-end
-
 ActiveAdmin.register Settings do
   actions :except => [ :index, :update, :edit, :show ]
   config.filters = false
@@ -17,16 +10,38 @@ ActiveAdmin.register Settings do
       redirect_to list_admin_settings_path
     end
   end
-    
+  
   collection_action :list do
-    @page_title = "Settings"
+    @page_title = "All Settings"
     
     @settings_hash = Settings.defaults.merge(Settings.all)
     @settings = []
-    @settings_hash.each { |k, v| @settings << SettingValue.new(k.to_sym, v) }
+    @settings_hash.each { |k, v| @settings << Settings::Value.new(k.to_sym, v) }
+  end
+
+  action_item :only => :list do
+    link_to("Edit", change_admin_settings_path)
   end
   
-  #collection_action :set_value, :method => :post do
-  #  redirect_to {:action => :index}, :notice => "got the redirect"
-  #end
+  collection_action :change do
+    @page_title = "Edit Settings"
+    
+    @settings_hash = Settings.defaults.merge(Settings.all)
+    @settings = []
+    @settings_hash.each { |k, v| @settings << Settings::Value.new(k.to_sym, v) }
+  end  
+  
+  collection_action :set_values, :method => :post do
+    new_settings = params[:settings]
+    new_settings.each do |k, v|
+      # Only update things that aren't inheriting defaults
+      if Settings.all.include? k
+        Settings[k] = v
+      elsif Settings.defaults[k] != v
+        Settings[k] = v
+      end
+    end
+    
+    redirect_to admin_settings_path
+  end
 end
--- app/models/settings.rb
+++ app/models/settings.rb
@@ -1,4 +1,12 @@
 # -*- encoding : utf-8 -*-
 class Settings < RailsSettings::CachedSettings
 	attr_accessible :var
+
+  class Value < Struct.new(:key, :val)
+    include ActiveModel::Conversion  
+    extend ActiveModel::Naming
+    extend ActiveModel::Translation
+    
+    def persisted?; false; end
+  end
 end
