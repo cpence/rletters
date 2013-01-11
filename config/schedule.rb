20db509463fa1a9254d83e4db1ec120f9cb70b8c
Charles Pence <charles@charlespence.net>
2013-01-11 01:42:32 -0500

    Update Whenever schedule file.
    The PATH stuff should no longer be needed, as it's calling jobs with
    bash -l -c, which should get the deploy user's PATH.  Also, we're
    redirecting all the logs to files, which means we don't need to set
    MAILTO anymore.  That fixes the first bad config dependency.

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,19 +2,12 @@
 
 set :output, {:standard => 'log/cron.log', :error => 'log/cron_error.log'}
 
-#### FIXME FIXME FIXME
-env :PATH, "#{YAML.load_file(File.expand_path('../app_config.yml', __FILE__))['all_environments']['ruby_path']}:/usr/local/bin:/usr/local/sbin:/usr/bin:/usr/sbin:/bin:/sbin"
-env :MAILTO, "#{YAML.load_file(File.expand_path('../app_config.yml', __FILE__))['all_environments']['app_email']}"
-
-
-job_type :env_command, "cd :path && RAILS_ENV=:environment bundle exec :task :output"
-
 every 1.hours do
   rake "db:sessions:expire"
   rake "db:downloads:expire"
 end
 
 every :reboot do
-  env_command "script/delayed_job restart"
+  script "delayed_job restart"
 end
 
