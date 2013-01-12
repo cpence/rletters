167c24d88e8c5d2b77a893dc642544170d6aebe9
Charles Pence <charles@charlespence.net>
2013-01-11 20:13:04 -0500

    Start to clean up the models before adding more.

--- config/initializers/00_default_settings.rb
+++ config/initializers/00_default_settings.rb
@@ -1,4 +1,6 @@
 # -*- encoding : utf-8 -*-
+require 'settings'
+
 Settings.defaults[:app_name] = 'RLetters'
 Settings.defaults[:app_email] = 'not@an.email.com'
 Settings.defaults[:app_domain] = 'not.a.web.site.com'
