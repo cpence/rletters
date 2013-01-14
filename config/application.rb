bcfa493f9e4f01b16fa18be4efc62d07a4f7af8e
Charles Pence <charles@charlespence.net>
2013-01-14 12:20:24 -0500

    More reorganizing of the models folder.

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module RLetters
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths << "#{config.root}/lib"
     config.autoload_paths << "#{config.root}/app/models/admin"
+    config.autoload_paths << "#{config.root}/app/models/dataset"
+    config.autoload_paths << "#{config.root}/app/models/user"
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
