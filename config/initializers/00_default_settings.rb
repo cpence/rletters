5afe9e55201a0f12c087d08571307d40efb0d950
Charles Pence <charles@charlespence.net>
2013-03-06 17:26:15 -0500

    Add some more specs.

--- config/initializers/00_default_settings.rb
+++ config/initializers/00_default_settings.rb
@@ -5,7 +5,7 @@ Settings.defaults[:app_name] = 'RLetters'
 Settings.defaults[:app_email] = 'not@an.email.com'
 Settings.defaults[:app_domain] = 'not.a.web.site.com'
 
-Settings.defaults[:solr_server_url] = 'http://localhost:8983'
+Settings.defaults[:solr_server_url] = 'http://localhost:8983/'
 Settings.defaults[:solr_timeout] = 120
 
 Settings.defaults[:mendeley_key] = ''
