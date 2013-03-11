4f32de39e79af61ec6424fb16ad87e75e0be2ce3
Charles Pence <charles@charlespence.net>
2013-03-11 15:08:52 -0400

    Fix the session store initializer so it won't error.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,8 @@
 # Be sure to restart your server when you modify this file.
 
 # Use cookies for sessions, keeping them out of the database
-RLetters::Application.config.session_store :cookie_store, key: "_#{Settings.app_name}_session"
+if ActiveRecord::Base.connection.tables.include?('settings')
+  RLetters::Application.config.session_store :cookie_store, key: "_#{Settings.app_name}_session"
+else
+  RLetters::Application.config.session_store :cookie_store, key: "_rletters_session"
+end
