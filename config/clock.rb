b25275af9490f5eefb3568bc790208646a85e84e
Charles Pence <charles@charlespence.net>
2013-03-11 11:33:22 -0400

    Mistake in the require directives here.

--- config/clock.rb
+++ config/clock.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
-require File.expand_path('./boot',        __FILE__)
-require File.expand_path('./environment', __FILE__)
+require File.expand_path('../boot',        __FILE__)
+require File.expand_path('../environment', __FILE__)
 require 'clockwork'
 
 include Clockwork
