4bb2c93aa5a5c10ea4148a426b71ee6be45e2331
Charles Pence <charles@charlespence.net>
2013-08-26 21:10:45 -0400

    Spec error page views (#38).

---
+++ spec/views/errors/404_spec.rb
@@ -0,0 +1,15 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe 'errors/404' do
+  it 'renders' do
+    expect {
+      render
+    }.to_not raise_error
+  end
+
+  it 'includes the error image' do
+    render
+    expect(rendered).to include(UploadedAsset.url_for('error-watermark'))
+  end
+end
