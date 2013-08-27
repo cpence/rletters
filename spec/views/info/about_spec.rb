f84346553591f35bc6b49cb6c8d64c246611493c
Charles Pence <charles@charlespence.net>
2013-08-26 22:15:11 -0400

    Spec the info controller views (#38).

---
+++ spec/views/info/about_spec.rb
@@ -0,0 +1,12 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe 'info/about' do
+
+  it 'renders' do
+    expect {
+      render
+    }.to_not raise_error
+  end
+
+end
---
+++ spec/views/info/tutorial_spec.rb
@@ -0,0 +1,12 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe 'info/tutorial' do
+
+  it 'renders' do
+    expect {
+      render
+    }.to_not raise_error
+  end
+
+end
