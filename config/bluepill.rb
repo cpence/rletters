a05953588350d4a51299dbceb5acd99ebdc8581d
Charles Pence <charles@charlespence.net>
2013-09-06 15:32:32 -0400

    More tweaks to Bluepill config.

--- config/bluepill.rb
+++ config/bluepill.rb
@@ -5,16 +5,12 @@ Bluepill.application('rletters') do |app|
   app.uid = app.gid = 'rletters_deploy'
 
   app.process('unicorn') do |proc|
-    proc.start_command = '/usr/local/bin/bundle exec unicorn -c /opt/rletters/root/config/unicorn.rb -E production -D'
+    proc.start_command = 'bundle exec unicorn -c /opt/rletters/root/config/unicorn.rb -E production -D'
     proc.stop_command = 'kill -QUIT {{PID}}'
     proc.restart_command = 'kill -USR2 {{PID}}'
 
     proc.pid_file = '/opt/rletters/root/tmp/pids/unicorn.pid'
     proc.working_dir = '/opt/rletters/root'
-
-    proc.start_grace_time = 1.minute
-    proc.stop_grace_time = 1.minute
-    proc.restart_grace_time = 1.minute
 
     proc.monitor_children do |child_process|
       child_process.stop_command = 'kill -QUIT {{PID}}'
@@ -27,16 +23,12 @@ Bluepill.application('rletters') do |app|
   app.process('clockwork') do |proc|
     clockwork_args = '--pid-dir=/opt/rletters/root/tmp/pids --log-dir=/opt/rletters/root/log --log -i 0 --clock=/opt/rletters/root/config/clock.rb'
 
-    proc.start_command = "/usr/local/bin/bundle exec clockworkd #{clockwork_args} start"
-    proc.stop_command = "/usr/local/bin/bundle exec clockworkd #{clockwork_args} stop"
-    proc.restart_command = "/usr/local/bin/bundle exec clockworkd #{clockwork_args} restart"
+    proc.start_command = "bundle exec clockworkd #{clockwork_args} start"
+    proc.stop_command = "bundle exec clockworkd #{clockwork_args} stop"
+    proc.restart_command = "bundle exec clockworkd #{clockwork_args} restart"
 
     proc.pid_file = '/opt/rletters/root/tmp/pids/clockworkd.0.pid'
     proc.working_dir = '/opt/rletters/root'
-
-    proc.start_grace_time = 30.seconds
-    proc.stop_grace_time = 30.seconds
-    proc.restart_grace_time = 30.seconds
   end
 
   delayed_job_queues = %w(maintenance ui analysis)
@@ -45,16 +37,12 @@ Bluepill.application('rletters') do |app|
     app.process("delayed_job-#{num}") do |proc|
       dj_args = "--pid-dir=/opt/rletters/root/tmp/pids -i #{num} --queue=#{delayed_job_queues[num]}"
 
-      proc.start_command = "/usr/local/bin/bundle exec bin/delayed_job #{dj_args} start"
-      proc.stop_command = "/usr/local/bin/bundle exec bin/delayed_job #{dj_args} stop"
-      proc.restart_command = "/usr/local/bin/bundle exec bin/delayed_job #{dj_args} restart"
+      proc.start_command = "bundle exec bin/delayed_job #{dj_args} start"
+      proc.stop_command = "bundle exec bin/delayed_job #{dj_args} stop"
+      proc.restart_command = "bundle exec bin/delayed_job #{dj_args} restart"
 
       proc.pid_file = "/opt/rletters/root/tmp/pids/delayed_job.#{num}.pid"
       proc.working_dir = '/opt/rletters/root'
-
-      proc.start_grace_time = 30.seconds
-      proc.stop_grace_time = 30.seconds
-      proc.restart_grace_time = 30.seconds
     end
   end
 
