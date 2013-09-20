99c0f9e1e669d613586eeda1d72269eefa464e7a
Charles Pence <charles@charlespence.net>
2013-09-20 11:01:48 -0400

    Change the name of the Resque server tab. [ci:skip]

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -4,10 +4,11 @@ require 'resque/server'
 # FIXME: For external job workers, we'll have to fix this.  But not just yet.
 Resque.redis = 'localhost:6379'
 
+# Thanks to Brian Clapper for the idea here.
 class Resque::Server
-  get '/admin' do
+  get '/back to admin interface' do
     redirect '/admin'
   end
 end
 
-Resque::Server.tabs << 'Admin'
+Resque::Server.tabs << 'Back to Admin Interface'
