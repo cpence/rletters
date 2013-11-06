3a1e22fc664043a5d8e58802106deec2eb561e79
Charles Pence <charles@charlespence.net>
2013-11-06 16:16:54 -0500

    Add a feature for the advanced search page.

---
+++ features/search/advanced_search.feature
@@ -0,0 +1,12 @@
+Feature: Advanced Search
+  As a visitor to the website
+  I want to be able to perform advanced searches
+  So that I can narrow down documents very precisely
+
+    Background:
+      Given I am not logged in
+
+    Scenario: I perform an advanced search
+      When I run an advanced search for the authors Mark Twain
+      Then I should see a list of articles
+        And the article list should contain Fenimore Cooper
--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -10,6 +10,12 @@ When /^I search for articles$/ do
   Capybara::RackTest::Form.new(page.driver, find('#search_form').native).submit(:name => nil)
 end
 
+When /^I run an advanced search for the ([a-z_]+) (.*)$/ do |field, content|
+  visit '/search/advanced/'
+  fill_in field, with: content
+  click_button 'Perform advanced search'
+end
+
 
 ### THEN ###
 Then /^I should see a list of articles$/ do
@@ -18,4 +24,9 @@ end
 
 Then /^I should see the number of articles found$/ do
   expect(page).to have_content(/\d+ articles found/)
+end
+
+Then /^the article list should contain (.*)$/ do |content|
+  element = find('table.document-list')
+  expect(element).to have_content(content)
 end
