598574c9454f5609b50db73f05d335e45201d13d
Charles Pence <charles@charlespence.net>
2013-12-04 09:46:47 -0500

    Reset all datasets to enabled when we migrate.

--- db/migrate/20131031190215_add_disabled_to_datasets.rb
+++ db/migrate/20131031190215_add_disabled_to_datasets.rb
@@ -1,6 +1,11 @@
 # -*- encoding : utf-8 -*-
 class AddDisabledToDatasets < ActiveRecord::Migration
-  def change
+  def up
     add_column :datasets, :disabled, :boolean
+    Datasets.all.each { |d| d.disabled = false; d.save! }
+  end
+
+  def down
+    remove_column :datasets, :disabled
   end
 end
