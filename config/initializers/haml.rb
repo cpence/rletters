d575d2ed17cbbc37cdcd122db6b0f125aec6605d
Charles Pence <charles@charlespence.net>
2013-12-07 16:07:50 -0500

    We're only setting config values to defaults for Haml, remove.

--- config/initializers/haml.rb
+++
@@ -1,8 +0,0 @@
-# -*- encoding : utf-8 -*-
-
-# Set a few Haml options, once it's loaded
-ActiveSupport.on_load(:action_vew) do
-  Haml::Template.options[:format] = :html5
-  Haml::Template.options[:encoding] = Encoding::UTF_8
-  Haml::Template.options[:ugly] = true
-end
