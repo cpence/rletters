22029fa8b6750cfd3781bc3def7142960582a32c
Charles Pence <charles@charlespence.net>
2013-12-08 21:56:26 -0500

    Fix Hash#to_utf8!.
    This wasn't actually working right; replace with a proper recursive
    implementation defined all the way down to Object.

--- lib/core_ext/hash/to_utf8.rb
+++ lib/core_ext/hash/to_utf8.rb
@@ -1,5 +1,33 @@
 # -*- encoding : utf-8 -*-
 
+# Ruby's base object class
+class Object
+  # Define a no-op to_utf8! method on all objects
+  #
+  # This enables easy recursion for Hash#to_utf8!.
+  def to_utf8!
+    self
+  end
+end
+
+# Ruby's standard string class
+class String
+  # Convert this string to UTF-8
+  def to_utf8!
+    force_encoding(Encoding::UTF_8)
+  end
+end
+
+# Ruby's standard array class
+class Array
+  # Convert all string encodings in this array to UTF-8
+  def to_utf8!
+    map! do |v|
+      v.to_utf8!
+    end
+  end
+end
+
 # Ruby's standard Hash class
 class Hash
   # Set all string encodings in the hash to UTF-8
@@ -9,41 +37,10 @@ class Hash
   #
   # @return [Hash] self
   def to_utf8!
-    force_hash_to_utf8(self)
+    each do |k, v|
+      v.to_utf8!
+      self[k] = v
+    end
     self
-  end
-
-  private
-
-  # Walk through a hash, setting any found string values to UTF-8
-  #
-  # @api private
-  # @return [undefined]
-  def force_hash_to_utf8(h)
-    h.each do |k, v|
-      if v.is_a? String
-        v.force_encoding(Encoding::UTF_8)
-      elsif v.is_a? Hash
-        force_hash_to_utf8(v)
-      elsif v.is_a? Array
-        force_array_to_utf8(v)
-      end
-    end
-  end
-
-  # Walk through an array, setting any found string values to UTF-8
-  #
-  # @api private
-  # @return [undefined]
-  def force_array_to_utf8(a)
-    a.each do |v|
-      if v.is_a? String
-        v.force_encoding(Encoding::UTF_8)
-      elsif v.is_a? Hash
-        force_hash_to_utf8(v)
-      elsif v.is_a? Array
-        force_array_to_utf8(v)
-      end
-    end
   end
 end
