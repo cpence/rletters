9083a9c8181624b55ff64c6a2c5cee3e98b9efb5
Charles Pence <charles@charlespence.net>
2014-05-27 10:20:56 -0400

    Fix syntax on skip_callback.

--- db/migrate/20140523162748_change_workflow_datasets_type.rb
+++ db/migrate/20140523162748_change_workflow_datasets_type.rb
@@ -2,8 +2,8 @@ class ChangeWorkflowDatasetsType < ActiveRecord::Migration
   def up
     # Skip the callback that requires the data to be in the new format when
     # migrating it for the first time
-    User.skip_callback(:save, :after_save, :workflow_datasets_from_ids)
-    User.skip_callback(:find, :after_find, :workflow_datasets_from_ids)
+    User.skip_callback(:save, :after, :workflow_datasets_from_ids)
+    User.skip_callback(:find, :after, :workflow_datasets_from_ids)
 
     # Save the current value of the parameter
     workflow_datasets = {}
