6eb26b67ab9787c0e03d4809ba526ec4cf76b66d
Charles Pence <charles@charlespence.net>
2014-05-27 10:15:16 -0400

    Try to fix the up-migration/callback linkage.

--- db/migrate/20140523162748_change_workflow_datasets_type.rb
+++ db/migrate/20140523162748_change_workflow_datasets_type.rb
@@ -1,5 +1,11 @@
 class ChangeWorkflowDatasetsType < ActiveRecord::Migration
   def up
+    # Skip the callback that requires the data to be in the new format when
+    # migrating it for the first time
+    User.skip_callback(:save, :before_save, :workflow_datasets_to_ids)
+    User.skip_callback(:save, :after_save, :workflow_datasets_from_ids)
+    User.skip_callback(:find, :after_find, :workflow_datasets_from_ids)
+
     # Save the current value of the parameter
     workflow_datasets = {}
     User.all.each do |u|
