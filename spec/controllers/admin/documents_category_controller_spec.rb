95e77e79620b67c65ea35af4d96be8a421a02e26
Charles Pence <charles@charlespence.net>
2014-12-23 14:08:12 -0600

    Clean up the test suite.
    A couple things didn't have tests, and a couple tests were in the
    wrong place.

---
+++ spec/controllers/admin/documents_category_controller_spec.rb
@@ -0,0 +1,53 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+RSpec.describe Admin::DocumentsCategoriesController, type: :controller do
+  # Normally I hate turning this on, but in ActiveAdmin, the view logic *is*
+  # defined in the same place where I define the controller.
+  render_views
+
+  before(:example) do
+    @category = create(:category)
+    @administrator = create(:administrator)
+    sign_in :administrator, @administrator
+  end
+
+  describe '#index' do
+    before(:example) do
+      get :index
+    end
+
+    it 'loads successfully' do
+      expect(response).to be_success
+    end
+
+    it 'includes one of the categories' do
+      expect(response.body).to include(@category.name)
+    end
+  end
+
+  describe '#show' do
+    before(:example) do
+      get :show, id: @category.to_param
+    end
+
+    it 'loads successfully' do
+      expect(response).to be_success
+    end
+  end
+
+  describe '#edit' do
+    before(:example) do
+      get :edit, id: @category.to_param
+    end
+
+    it 'loads successfully' do
+      expect(response).to be_success
+    end
+
+    it 'has checkboxes for the journals' do
+      expect(response.body).to have_selector('input#documents_category_journals_plos_neglected_tropical_diseases[name="documents_category[journals][]"]')
+    end
+  end
+
+end
---
+++ spec/lib/r_letters/datasets/add_search_spec.rb
@@ -0,0 +1,40 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+RSpec.describe RLetters::Datasets::AddSearch do
+  before(:context) do
+    @user = create(:user)
+    @dataset = create(:dataset, user: @user)
+  end
+
+  context 'with a basic search' do
+    before(:context) do
+      @called_sub_100 = false
+      @called_100 = false
+
+      @search = RLetters::Solr::Connection.search(q: '*:*', def_type: 'lucene')
+      @adder = described_class.new(@dataset, '*:*', nil, 'lucene', ->(p) {
+        if p < 100
+          @called_sub_100 = true
+        else
+          @called_100 = true
+        end
+      })
+
+      @adder.call
+    end
+
+    it 'fills in the dataset' do
+      expect(@dataset.entries.size).to eq(1502)
+    end
+
+    it 'sets the fetch tag' do
+      expect(@dataset.fetch).to be true
+    end
+
+    it 'calls the progress reporter' do
+      expect(@called_sub_100).to be true
+      expect(@called_100).to be true
+    end
+  end
+end
