15d60852fb26f45c10b82f7e31eaa43c355d3884
Charles Pence <charles@charlespence.net>
2017-04-11 22:43:19 -0500

    Rename 'helpers' directory (conflict).

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,5 @@
 ENV['RAILS_ENV'] ||= 'test'
-require_relative 'helpers/simplecov'
+require_relative 'support/simplecov'
 require_relative '../config/environment'
 require 'rails/test_help'
 
@@ -8,8 +8,8 @@ require 'mocha/mini_test'
 require 'webmock/minitest'
 
 # Require all of my test helpers and assertions
-(Dir[Rails.root.join('test', 'helpers', '**', '*_helper.rb')] +
-Dir[Rails.root.join('test', 'helpers', '**', 'assert_*.rb')]).each do |helper|
+(Dir[Rails.root.join('test', 'support', '**', '*_helper.rb')] +
+Dir[Rails.root.join('test', 'support', '**', 'assert_*.rb')]).each do |helper|
   require helper
 end
 
