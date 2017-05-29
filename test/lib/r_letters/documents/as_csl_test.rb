5d9768bfcd6ae4407aca4ee0200dfe5ec1ac2f9f
Charles Pence <charles@charlespence.net>
2017-05-29 10:58:35 -0500

    Port Documents utility code tests.

---
+++ test/lib/r_letters/documents/as_csl_test.rb
@@ -0,0 +1,59 @@
+require 'test_helper'
+
+class AsCSLTest < ActiveSupport::TestCase
+  test 'creates good citeproc items' do
+    doc = build(:full_document)
+    as_csl = RLetters::Documents::AsCSL.new(doc)
+    csl = as_csl.citeproc_item
+
+    assert_equal 'article-journal', csl.type
+    assert_equal 'Dickens', csl.author[0].family
+    assert_equal 'C.', csl.author[0].given
+    assert_equal 'A Tale of Two Cities', csl.title
+    assert_equal 'Actually a Novel', csl.container_title
+    assert_equal Date.new(1859), csl.issued.to_date
+    assert_equal '1', csl.volume
+    assert_equal '1', csl.issue
+    assert_equal '1', csl.page
+  end
+
+  test 'formats citations correctly' do
+    style = stub(style: <<eos)
+<style xmlns="http://purl.org/net/xbiblio/csl"  class="in-text" version="1.0">
+  <info>
+    <id />
+    <title />
+    <updated>2009-08-10T04:49:00+09:00</updated>
+  </info>
+  <citation>
+    <layout>
+      <names variable="author">
+        <name />
+      </names>
+    </layout>
+  </citation>
+  <bibliography>
+    <layout>
+      <names variable="author">
+        <name />
+      </names>
+    </layout>
+  </bibliography>
+</style>
+eos
+
+    doc = build(:full_document)
+    as_csl = RLetters::Documents::AsCSL.new(doc)
+
+    assert_equal 'C. Dickens', as_csl.entry(style)
+  end
+
+  test 'raises if you try to format with a weird style' do
+    doc = build(:full_document)
+    as_csl = RLetters::Documents::AsCSL.new(doc)
+
+    assert_raises(NameError) do
+      as_csl.entry(37)
+    end
+  end
+end
---
+++ test/lib/r_letters/documents/as_open_url_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class AsOpenURLTest < ActiveSupport::TestCase
+  test 'creates good OpenURL params' do
+    doc = build(:full_document)
+    params = RLetters::Documents::AsOpenURL.new(doc).params
+
+    assert_equal 'ctx_ver=Z39.88-2004&rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3A' \
+        'mtx%3Ajournal&rft.genre=article&' \
+        'rft_id=info:doi%2F10.5678%2Fdickens&' \
+        'rft.atitle=A+Tale+of+Two+Cities&rft.title=Actually+a+Novel&' \
+        'rft.date=1859&rft.volume=1&rft.issue=1&rft.spage=1&rft.aufirst=C.&' \
+        'rft.aulast=Dickens', params
+  end
+end
