5a4654208124598166f036c5ff71ae0b3f3d3ac9
Charles Pence <charles@charlespence.net>
2018-04-10 10:40:32 -0500

    Whoops, forgot to add a file on that last commit.

---
+++ config/initializers/bootstrap_forms.rb
@@ -0,0 +1,16 @@
+# Override the default 'field_with_errors' behavior in Rails to produce the
+# kind of invalid markers we want in Bootstrap 4.
+Rails.application.config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+  class_attr_index = html_tag.index 'class="'
+
+  if class_attr_index
+    html_tag.insert class_attr_index+7, 'is-invalid '
+  else
+    class_attr_index = html_tag.index "class='"
+    if class_attr_index
+      html_tag.insert class_attr_index+7, 'is-invalid '
+    else
+      html_tag.insert html_tag.index('>'), ' class="is-invalid"'
+    end
+  end
+end
