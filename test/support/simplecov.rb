8760e107dc32526f6a9aa692e992778bf4084e53
Charles Pence <charles@charlespence.net>
2018-05-09 11:25:58 -0500

    Group test results in local coverage files.

--- test/support/simplecov.rb
+++ test/support/simplecov.rb
@@ -12,6 +12,15 @@ if ENV['TRAVIS'] || ENV['COVERAGE']
     add_filter '.haml'
     add_filter '.erb'
     add_filter '.builder'
+
+    add_group 'Controllers', 'app/controllers'
+    add_group 'Helpers', 'app/helpers'
+    add_group 'Jobs', 'app/jobs'
+    add_group 'Mailers', 'app/mailers'
+    add_group 'Models', 'app/models'
+    add_group 'Core Extensions', 'lib/core_ext'
+    add_group 'Library', 'lib/r_letters'
+    add_group 'Rake Tasks', 'lib/tasks'
   end
 
   SimpleCov.merge_timeout 3600
