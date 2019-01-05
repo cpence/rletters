1538b036c02127d642e9783b435826b05d30658d
Charles Pence <charles@charlespence.net>
2019-01-05 14:59:08 +0100

    Add some custom Sentry filters to take junk out of our logs.

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+if ENV['SENTRY_DSN'].present?
+  Raven.configure do |config|
+    # Don't send a number of exceptions on to Sentry
+    config.excluded_exceptions += [
+      # Produced by the user when they request to kill a job
+      'JobKilledError',
+      # Produced by DelayedJob when a job runs for too long
+      'Delayed::WorkerTimeout',
+      # Produced by Heroku and other process managers, not indicative of anything
+      # happening in our code
+      'SignalException'
+    ]
+  end
+end
