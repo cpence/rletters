809a3740b6fb768f0e5e8dd2f7789d708d51caf0
Charles Pence <charles@charlespence.net>
2019-10-06 14:21:41 +0200

    Collapse migrations; Paperclip is making it impossible to up-migrate.

--- db/migrate/20180501135704_rename_tables.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class RenameTables < ActiveRecord::Migration[5.2]
-  def change
-    rename_table :admin_uploaded_assets, :admin_assets
-    rename_table :admin_markdown_pages, :admin_snippets
-  end
-end
--- db/migrate/20180511195634_remove_fetch_from_datasets.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveFetchFromDatasets < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :datasets, :fetch, :boolean, default: false
-  end
-end
